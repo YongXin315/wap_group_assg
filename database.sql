9bf0357e7ea3b520a98969f3bb45408fae3e9128
YongXin315 <xinchong909@gmail.com>
2025-06-25 21:38:04 +0800

    Initial commit with database.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE IF NOT EXISTS wap_system;
 USE wap_system;
 
-CREATE TABLE students (
+CREATE TABLE student (
     id INT AUTO_INCREMENT PRIMARY KEY,
     student_id VARCHAR(50) NOT NULL UNIQUE,
     student_email VARCHAR(255) NOT NULL UNIQUE,
-    student_name VARCHAR(255) NOT NULL,
+    student_name VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
@@ -18,12 +18,25 @@ CREATE TABLE admin (
 
 CREATE TABLE room (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    room_name VARCHAR(255) NOT NULL UNIQUE,
+    room_name VARCHAR(100) NOT NULL UNIQUE,
     `block` VARCHAR(50) NOT NULL,
     floor VARCHAR(50) NOT NULL,
-    `type` VARCHAR(255) NOT NULL,
-    capacity VARCHAR(255) NOT NULL,
-    amenities VARCHAR(255) NOT NULL,
-    `status` VARCHAR(50) NOT NULL
+    `type` VARCHAR(100) NOT NULL,
+    min_capacity INT NOT NULL,
+    max_capacity INT NOT NULL,
+    amenities VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE booking (
+    booking_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id VARCHAR(50) NOT NULL,
+    room_name VARCHAR(100) NOT NULL,
+    booking_date date NOT NULL,
+    start_time 
+    end_time
+    booking_purpose TEXT,
+    number_of_people VARCHAR(50) NOT NULL,
+    `status` VARCHAR(50) NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES student(student_id),
+    FOREIGN KEY (room_name) REFERENCES room(room_name)
+);
