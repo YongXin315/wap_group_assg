2c34e3227fbf6679cf9d9446d2a796fa52f4996e
YongXin315 <xinchong909@gmail.com>
2025-06-27 18:06:36 +0800

    Admin side update

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,24 +1,3 @@
-<?php
-session_start();
+<!-- admin: booking management dashboard -->
 
-// inactivity timeout (1 day for admin)
-// $timeout = 86400;
-$timeout = 20;
-
-// if admin not logged in, redirect to login
-if (!isset($_SESSION['admin_id'])) {
-    header("Location: index.php");
-    exit;
-}
-
-// check inactivity
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout) {
-    session_unset();
-    session_destroy();
-    // Redirect with timeout flag
-    header("Location: index.php?timeout=1");
-    exit;
-}
-// Update last activity
-$_SESSION['last_activity'] = time();
-?>
+<?php require 'admin_auth.php'; ?>
---
+++ admin_manage_room.php
@@ -0,0 +1,3 @@
+<!-- admin: manage room and update data -->
+
+<?php require 'admin_auth.php'; ?>
