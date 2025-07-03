2fc4797699e68f9d4b0bc9ec6513128f6321d95a
tjianyang <benjamintjy04@gmail.com>
2025-07-03 18:13:34 +0800

    Update confirmbooking.php

--- confirmbooking.php
+++ confirmbooking.php
@@ -1,5 +1,15 @@
-<?php session_start(); ?>
-<?php include './component/header.php'; ?>
+<?php
+session_start();
+
+// Check if user is logged in, if not redirect to login page
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+include 'components/header.php';
+?>
+
 
 <link rel="stylesheet" href="./assests/main.css">
 
